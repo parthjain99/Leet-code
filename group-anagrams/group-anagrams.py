d13521dd9e9b4dce1b35e2c3b1bb88b1d8b96238
Parth Hasmukh Jain <91812749+parthjain99@users.noreply.github.com>
2021-10-21 10:50:11 -0400

    Time: 161 ms (22.14%), Space: 17.1 MB (94.00%) - LeetHub

---
+++ group-anagrams/group-anagrams.py
@@ -0,0 +1,13 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:    
+        h={}
+        for word in strs:
+            sortwords="".join(sorted(word))
+            if sortwords not in h:
+                h[sortwords]=[word]
+            else:
+                h[sortwords].append(word)
+        final=[]
+        for value in h.values():
+            final.append(value)
+        return final
