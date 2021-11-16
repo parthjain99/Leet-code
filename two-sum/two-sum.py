6b1ffd03b078546c4d3545d3a0df69aef1c8cfcc
Parth Hasmukh Jain <91812749+parthjain99@users.noreply.github.com>
2021-11-16 09:27:47 -0500

    Time: 60 ms (79.50%), Space: 15.4 MB (41.60%) - LeetHub

--- two-sum/two-sum.py
+++ two-sum/two-sum.py
@@ -1,12 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        a={}
-        for i,val in enumerate(nums):
-            y=target-val
-            if y in a:
-                z=a[y]
-                return(z,i)
-            else:
-                a[val]=i
-            
-            
+        dmap={}
+        for i,n in enumerate(nums):
+            diff=target-n
+            if diff in dmap:
+                return[dmap[diff],i]               
+            dmap[n]=i
