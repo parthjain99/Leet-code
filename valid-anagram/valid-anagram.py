8f0d84ab06f8141b809631ac81005ec0bcc42971
Parth Hasmukh Jain <91812749+parthjain99@users.noreply.github.com>
2021-11-20 09:43:05 -0500

    Time: 79 ms (14.84%), Space: 14.6 MB (57.15%) - LeetHub

--- valid-anagram/valid-anagram.py
+++ valid-anagram/valid-anagram.py
@@ -1,33 +1,13 @@
+import collections
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s="".join(sorted(s))
-        t="".join(sorted(t))
-        if s==t:
-            return True
-        else:
-            False
-        
-#         maps={}
-#         mapt={}
-#         for i in s:
-#             maps{i}= maps.get(i,0)+1
-
-#         for i in t:
-#             mapt{i}= mapt.get(i,0)+1
-
-        
-#         return maps==mapt
-                
-        
-        
-        
-        
-        
-#         i=list(s)
-#         i.sort()
-#         anagram=list(t)
-#         anagram.sort()
-#         if i==anagram:
-#             return True
-#         else:
-#             return False
+        a=collections.Counter(s)
+        print(a)
+        if len(s)!=len(t):
+            return False
+        for i in t:
+            if i in a and a[i]>0:
+                a[i]-=1
+            else:
+                return False
+        return True
